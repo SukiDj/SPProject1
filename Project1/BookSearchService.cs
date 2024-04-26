e8e4e33a99aa195bfa70b618ef599398f9e6a55a
Aleksandar Djordjevic <djordjevic.1.aleksandar02@gmail.com>
2024-04-26 20:17:40 +0200

    first commit

---
+++ Project1/BookSearchService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace Project1
+{
+    public class BookSearchService
+    {
+        private string apiUrl = "https://api.nytimes.com/svc/books/v3/reviews.json";
+        private string apiKey = "7DQNwuHGWsu87ZfjCmO9Pg4sSzdmDPAs";
+
+        public List<Book> SearchBooksByAuthor(string author)
+        {
+            List<Book> books = new List<Book>();
+
+            string query = $"?author={author}&api-key={apiKey}";
+            string fullUrl = apiUrl + query;
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                HttpResponseMessage apiResponse = httpClient.GetAsync(fullUrl).Result;
+                if (apiResponse.IsSuccessStatusCode)
+                {
+                    string responseBody = apiResponse.Content.ReadAsStringAsync().Result;
+                    dynamic jsonData = JsonConvert.DeserializeObject(responseBody);
+                    foreach (var item in jsonData.results)
+                    {
+                        Book book = new Book
+                        {
+                            Url = item.url,
+                            PublicationDate = item.publication_dt,
+                            Title = item.book_title,
+                            Author = item.book_author,
+                            Summary = item.summary
+                        };
+                        books.Add(book);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {apiResponse.StatusCode}");
+                }
+            }
+
+            return books;
+        }
+    }
+}
---
+++ Project1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Project1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (HttpListener listener = new HttpListener())
+        {
+            listener.Prefixes.Add("http://localhost:8080/");
+            listener.Start();
+            Console.WriteLine("Web server running...");
+
+            while (true)
+            {
+                HttpListenerContext context = listener.GetContext();
+                ThreadPool.QueueUserWorkItem((state) =>
+                {
+                    HttpListenerRequest request = context.Request;
+                    HttpListenerResponse response = context.Response;
+
+                    Console.WriteLine($"Request received: {request.Url}");
+
+                    // Procesiranje zahteva
+                    string author = request.QueryString["author"];
+                    if (!string.IsNullOrEmpty(author))
+                    {
+                        BookSearchService bookSearchService = new BookSearchService();
+                        List<Book> books = bookSearchService.SearchBooksByAuthor(author);
+
+                        // Slanje odgovora klijentu
+                        string responseBody = Newtonsoft.Json.JsonConvert.SerializeObject(books);
+                        byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseBody);
+                        response.ContentLength64 = buffer.Length;
+                        Stream output = response.OutputStream;
+                        output.Write(buffer, 0, buffer.Length);
+                        output.Close();
+                    }
+                    else
+                    {
+                        response.StatusCode = (int)HttpStatusCode.BadRequest;
+                        response.StatusDescription = "Bad Request";
+                        response.Close();
+                    }
+                }, null);
+            }
+        }
+    }
+}
